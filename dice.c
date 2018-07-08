befe139bb991d804862777eec21d3573d54a4ab0
shirome-nak <s1240233@u-aizu.ac.jp>
2018-07-08 20:12:55 +0900

    Won_lost message modified

--- dice.c
+++ dice.c
@@ -4,7 +4,12 @@
 
 int main(){
   int die1,die2;
+  char name[256];
   srand(time(NULL)); //initialize
+
+  printf("What is your name?\n->");
+  scanf("%s", name);
+  printf("Hello, %s.\n", name);
 
   printf("Rolling the dice...\n");
   die1 = (rand()%6+1);
@@ -12,6 +17,9 @@ int main(){
   printf("Die 1: %d\n",die1);
   printf("Die 2: %d\n",die2);
   printf("Total value: %d\n",die1+die2);
+
+  if(die1+die2 > 7) printf("%s won!\n",name);
+  else printf("%s lose!\n",name);
 
   return 0;
 }
