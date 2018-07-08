c480d3a879b2b51c036ef958790b43e13e039fbd
shirome-nak <s1240233@u-aizu.ac.jp>
2018-07-08 18:02:15 +0900

    add new file

---
+++ dice.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int die1,die2;
+  srand(time(NULL)); //initialize
+
+  printf("Rolling the dice...\n");
+  die1 = (rand()%6+1);
+  die2 = (rand()%6+1);
+  printf("Die 1: %d\n",die1);
+  printf("Die 2: %d\n",die2);
+  printf("Total value: %d\n",die1+die2);
+
+  return 0;
+}
